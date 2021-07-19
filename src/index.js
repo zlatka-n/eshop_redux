2ca1e56ec790ef2d3e27e07e9bc1b1c23959aee5
zlatka-n <zla.b.ngu@gmail.com>
2021-07-19 16:05:45 +0200

    redux store connected

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
