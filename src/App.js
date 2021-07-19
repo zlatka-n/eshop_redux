d90e4c35a73ff3e83fa3f7f18f763d579e5cafa8
zlatka-n <zla.b.ngu@gmail.com>
2021-07-19 12:57:19 +0200

    react-router set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./index.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Basket from "./components/Basket";
+import NavBar from "./components/NavBar";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <h5>Hello</h5>
+      <Router>
+        <NavBar />
+
+        <Switch>
+          <Route path="/basket" exact>
+            <Basket />
+          </Route>
+          <Route path="/" exact>
+            <MainPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
