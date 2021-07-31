895714f291702267bdfe703b6f8db5ccd971422f
zlatka-n <zla.b.ngu@gmail.com>
2021-07-31 22:00:41 +0200

    react icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaShoppingBasket } from "react-icons/fa";
+import { ImBooks } from "react-icons/im";
 
 function NavBar() {
   return (
     <header>
       <div className="navBar-container">
         <Link to="/" className="home">
-          Home
+          <ImBooks></ImBooks>Book store
         </Link>
 
-        <Link to="/basket">Shopping-Basket</Link>
+        <Link to="/basket">
+          <FaShoppingBasket></FaShoppingBasket>
+        </Link>
       </div>
     </header>
   );
