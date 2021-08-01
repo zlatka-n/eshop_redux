b41e8f5557310f112009ec1a769cba0cc2e5b265
zlatka-n <zla.b.ngu@gmail.com>
2021-08-01 09:22:37 +0200

    counter qty next to basket icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingBasket } from "react-icons/fa";
 import { ImBooks } from "react-icons/im";
+import { connect } from "react-redux";
+import _ from "lodash";
+function NavBar(props) {
+  //fn showing number of items next to the basket icon
+  const getNoItems = () => {
+    const mapCart = props.cart;
+    const showNoItems = _.map(mapCart, "quantity");
 
-function NavBar() {
+    //sum of all items
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const sumQty = showNoItems.reduce(reducer, 0);
+
+    const showInNavBar = () => (sumQty > 0 ? sumQty : null);
+
+    return <React.Fragment>{showInNavBar()}</React.Fragment>;
+  };
+
   return (
     <header>
       <div className="navBar-container">
@@ -12,11 +27,20 @@ function NavBar() {
         </Link>
 
         <Link to="/basket">
-          <FaShoppingBasket></FaShoppingBasket>
+          <div>
+            {getNoItems()}
+            <FaShoppingBasket></FaShoppingBasket>
+          </div>
         </Link>
       </div>
     </header>
   );
 }
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.getBook.buy,
+  };
+};
+
+export default connect(mapStateToProps)(NavBar);
