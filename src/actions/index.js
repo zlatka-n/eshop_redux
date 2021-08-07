397f86846b3615dbca25abdb21876dbd3a9cc0e8
zlatka-n <zla.b.ngu@gmail.com>
2021-08-07 12:31:12 +0200

    actions

---
+++ src/actions/index.js
@@ -0,0 +1,37 @@
+export const buyItem = (id) => {
+  return {
+    type: "BUY_ITEM",
+    payload: {
+      id: id,
+      // quantity: quantity,
+      // totalPrice: price * quantity,
+    },
+  };
+};
+
+export const deleteItem = (id) => {
+  return {
+    type: "DELETE_ITEM",
+    payload: {
+      id: id,
+    },
+  };
+};
+
+export const incrementQty = (id) => {
+  return {
+    type: "INCREMENT_QTY",
+    payload: {
+      id: id,
+    },
+  };
+};
+
+export const decrementQty = (id) => {
+  return {
+    type: "DECREMENT_QTY",
+    payload: {
+      id: id,
+    },
+  };
+};
