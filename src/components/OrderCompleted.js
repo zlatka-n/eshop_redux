f1d9af8f0f12b59a12bf6512609404f79aad6825
zlatka-n <zla.b.ngu@gmail.com>
2021-08-14 17:51:59 +0200

    order confirmed

--- src/components/OrderCompleted.js
+++ src/components/OrderCompleted.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "../css/order.css";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+
 function OrderCompleted() {
   return (
-    <div>
-      Thank your for your purchase. Your order will be dispatched within 2 days.
+    <div className="order-container">
+      <div className="completedOrder-title">
+        <span className="completedIcon">
+          <AiOutlineCheckCircle className="completedIcon" />
+        </span>
+        <span>Order confirmed</span>
+      </div>
+      <div>
+        Thank your for your purchase. Your order will be dispatched within 2
+        days.
+      </div>
     </div>
   );
 }
