2722c41128e3538e06df761f4ec99a8b412de1f0
zlatka-n <zla.b.ngu@gmail.com>
2021-08-21 09:06:57 +0200

    typos corrected

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,9 +9,9 @@ function AboutUs() {
         <p>Hello,</p>
         <p>
           thank you for visiting my bookstore page. I created this website with
-          the intention to improve my React / Redux skills. It took me about
-          four weeks to finish this bookstore, and I can't say how much I
-          learned from this project (and how happy I am happy having finished
+          the intention to primarily improve my React and Redux skills. It took
+          me about four weeks to finish this bookstore, and I can't say how much
+          I learned from this project (and how happy I am happy having finished
           it). Firstly, I pushed myself to use effectively Stackoverflow, think
           more about nested data structures and finally learn more of css. The
           latest was my most procrastinated front-end thing to learn in depth.
@@ -20,7 +20,7 @@ function AboutUs() {
           screen sizes.
         </p>
         <p>
-          Below I include my techstack that I used and also my github account.
+          Below I include my tech stack that I used and also my github account.
           Don't hesitate to contact me either by phone +420735204082 or email
           zlatka094@seznam.cz. Once again thank you for visiting my page.
         </p>
